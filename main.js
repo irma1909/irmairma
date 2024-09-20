e40a0653fe41746d4206598bff3d3dac2415cc04
irma1909 <irmawally1909@gmail.com>
2024-09-20 15:36:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// hamburger menu
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden')
+});
+
+// scroll navbar
+window.onscroll = function (){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.scrollY > fixedNav){
+        header.classList.add('navbar-fixed');
+        header.classList.remove('absolute');
+    } else {
+        header.classList.remove('navbar-fixed');
+        header.classList.add('absolute')
+    }
+}
